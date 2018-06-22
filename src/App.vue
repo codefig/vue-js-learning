f10a165187f56013841afa72a11197da4e702cae
Alaran Moshood <geekyshood@gmail.com>
2018-06-22 03:05:58 +0100

    added local and global filters

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <h1>Filters and Mixins </h1>
+               <p> {{text | toUpperCase|to-lowercase }}</p>
             </div>
         </div>
     </div>
@@ -11,7 +12,15 @@
 <script>
 
     export default {
-        
+        data () {
+        	return {
+        		text : "Hello world",
+        	}
+        }, filters :{
+        	toUpperCase(value) {
+        		return value.toUpperCase();
+        	} 
+        }
     }
 </script>
 
