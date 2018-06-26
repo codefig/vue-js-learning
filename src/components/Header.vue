69cc20008e6681c24bcaeba9be996625823098dc
Alaran Moshood <geekyshood@gmail.com>
2018-06-26 03:50:09 +0100

    added the header file

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+	<ul class="nav navbar-nav">
+		<li class="active">
+			<!-- <a href="#">Home</a> -->
+			<router-link to="/">Home</router-link>
+		</li>
+		<li>
+			<router-link to="/user">Users</router-link>
+		</li>
+	</ul>
+</template>
