14cc87c4a19932985db0eeac313c5c7dc160d0e7
Alaran Moshood <geekyshood@gmail.com>
2018-06-26 03:56:52 +0100

    router-link now working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
-	<ul class="nav navbar-nav">
-		<li class="active">
-			<!-- <a href="#">Home</a> -->
-			<router-link to="/">Home</router-link>
-		</li>
-		<li>
-			<router-link to="/user">Users</router-link>
-		</li>
-	</ul>
+	<div class="navbar">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Title</a>
+			<ul class="nav navbar-nav">
+				<li class="active">
+					<router-link to="/">Home</router-link>
+				</li>
+				<li>
+					<router-link to="/user"> Users </router-link>
+				</li>
+			</ul>
+		</div>
+	</div>
 </template>
