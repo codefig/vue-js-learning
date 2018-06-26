9b361b100efd2c03acd73722dabdeb79948e6494
Alaran Moshood <geekyshood@gmail.com>
2018-06-26 03:50:32 +0100

    trying to use the navbar and router-link

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <h1> Routing </h1>
+               <hr>
+               <app-header></app-header>
+               <router-view></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+	import Header from './components/Header.vue';
     export default {
-        
+        components : {
+        	appHeader : Header,
+        }
     }
 </script>
 
---
+++ src/components/User.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="col-sm-12 col-xs-6">
+		<h1> User page </h1>
+
+		Lorem user dolor sit amet, consectetur adipisicing elit. Itaque deleniti veniam atque eos, unde, sequi eveniet ab magni architecto maxime soluta voluptate ea! Porro obcaecati temporibus aliquam eum quia pariatur!
+	</div>
+</template>
