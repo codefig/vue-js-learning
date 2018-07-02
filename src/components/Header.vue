a5cbca0c163d96242d26ba60a3ea1c238cc17111
Alaran Moshood <geekyshood@gmail.com>
2018-07-02 10:00:00 +0100

    active class styling not working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,8 @@
 		<div class="container-fluid">
 			<a class="navbar-brand" href="#">Title</a>
 			<ul class="nav navbar-nav">
-				<li class="active">
-					<router-link to="/">Home</router-link>
-				</li>
-				<li>
-					<router-link to="/user"> Users </router-link>
-				</li>
+				<router-link to="/" tag="li" active-class="active">Home</router-link>
+				<router-link to="/user" tag="li" active-class="active"> Users </router-link> 
 			</ul>
 		</div>
 	</div>
