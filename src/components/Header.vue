9ebc30a84f3d8d4fdde1755ec2c266a367c80c0b
Alaran Moshood <geekyshood@gmail.com>
2018-07-02 10:44:44 +0100

    watching for parameter changes using watch

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,9 @@
 		<div class="container-fluid">
 			<a class="navbar-brand" href="#">Title</a>
 			<ul class="nav navbar-nav">
-				<router-link to="/" tag="li" active-class="active">Home</router-link>
-				<router-link to="/user/10" tag="li" active-class="active"> Users </router-link> 
+				<router-link to="/" tag="li" active-class="active"><a>Home</a></router-link>
+				<router-link to="/user/1" tag="li" active-class="active"><a> Users 1</a> </router-link> 
+				<router-link to="/user/2" tag="li" active-class="active"><a> Users 2</a> </router-link> 
 			</ul>
 		</div>
 	</div>
